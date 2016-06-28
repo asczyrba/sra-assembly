7be5e389c746baedd6bda16c4813dea02dbef766
Ubuntu <ubuntu@ip-10-10-0-48.eu-west-1.compute.internal>
2016-06-28 13:58:48 +0000

    work in subdir

--- docker-prodigal/run_prodigal.sh
+++ docker-prodigal/run_prodigal.sh
@@ -3,6 +3,9 @@
 S3_BUCKET=$1
 ID=$2
 
+mkdir $ID
+cd $ID
+
 aws s3 cp $S3_BUCKET/final.contigs.fa - | prodigal -a ${ID}_genes.faa -d ${ID}_genes.fna -f gff -o ${ID}_genes.gff -p meta
 
 agrep -d '\>' 'partial=00' ${ID}_genes.faa > ${ID}_genes_complete.faa
@@ -12,4 +15,6 @@ agrep -v -d '\>' 'partial=00' ${ID}_genes.fna > ${ID}_genes_partial.fna
 
 aws --region=us-west-2 s3 sync . $S3_BUCKET/
 
-rm -v ${ID}_genes*
+cd ..
+rm -v -rf ${ID}
+
